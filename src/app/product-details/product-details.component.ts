db3def2ef072885eadaaabf003ae8496bbd09028
VictorJP <victor.jaimes.puente@gmail.com>
2020-07-10 23:28:13 -0500

    added routing

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
 
 @Component({
   selector: 'app-product-details',
