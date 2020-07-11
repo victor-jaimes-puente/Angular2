a57d660a47d462f3f3789c8bcc3e8780562372b2
VictorJP <victor.jaimes.puente@gmail.com>
2020-07-10 22:44:00 -0500

    added notify me functionality x

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
-
+import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
@@ -8,8 +8,11 @@ import { Input } from '@angular/core';
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
-  constructor() { }
+  @Output() notify = new EventEmitter();
 
+
+
+  constructor() { }
   ngOnInit(): void {
   }
 
